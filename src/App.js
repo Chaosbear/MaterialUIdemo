6b09779796e67fcc9774a4402613f4b41186223b
Chaosbear <sukrit.chatmeeboon@gmail.com>
2021-10-27 13:59:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, Link } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
 
-function App() {
+import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const App = () => {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon} />
+          <Typography variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm" style={{marginTop: '10px'}}>
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Hello everyone, This is a photo album that I learned from JavaScript Mastery Channel on Youtube. If you are interested in this project, please go to this&nbsp;
+              <Link href="https://www.youtube.com/watch?v=Xoz31I1FuiY&t=1330s" target="_blank" rel="noreferrer noopener">
+                link
+              </Link>
+              &nbsp;and please subcribe their channel.
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justifyContent="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary action
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4} justifyContent="center">
+          {cards.map((card) => (
+            <Grid item key={card} xs={10} sm={5} md={4}>
+              <Card className={classes.Card}>
+                <CardMedia 
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image Title"
+                />
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is media card. You can use this section to describe the content
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">View</Button>
+                  <Button size="small" color="primary">Edit</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+          Footer detail
+        </Typography>
+      </footer>
+    </>
   );
 }
 
